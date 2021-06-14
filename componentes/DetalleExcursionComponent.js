f0e042abd4a60ebd8fae5ef15c95c36d66bf38d6
Nerea <nerea.im1896@gmail.com>
2021-06-14 18:47:23 +0200

    Ejercicio Formularios y modals con Redux

--- componentes/DetalleExcursionComponent.js
+++ componentes/DetalleExcursionComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, StyleSheet} from 'react-native';
-import { Card, Icon } from 'react-native-elements';
-import { baseUrl } from '../comun/comun';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet } from 'react-native';
+import { Card, Icon, Input, Button, Rating } from 'react-native-elements';
+import { baseUrl, colorGaztaroaOscuro } from '../comun/comun';
 import { connect } from 'react-redux';
-import { postFavorito } from "../redux/ActionCreators";
+import { postFavorito, postComentario } from "../redux/ActionCreators";
 
 const mapStateToProps = state => {
     return {
@@ -14,8 +14,8 @@ const mapStateToProps = state => {
   }
 
 const mapDispatchToProps = dispatch => ({
-  postFavorito: (excursionId) => dispatch(postFavorito(excursionId))
-})
+  postFavorito: (excursionId) => dispatch(postFavorito(excursionId)),
+  postComentario: (comentario) => dispatch(postComentario(comentario))})
 
 function RenderExcursion(props) {
 
@@ -30,14 +30,24 @@ function RenderExcursion(props) {
               <Text style={{margin: 20}}>
                 {excursion.descripcion}
               </Text>
-              <Icon
-                raised
-                reverse
-                name={ props.favorita ? 'heart' : 'heart-o'}
-                type='font-awesome'
-                color='#f50'
-                onPress={() => props.favorita ? console.log('La excursión ya se encuentra entre las favoritas') : props.onPress()}
-                />
+              <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+                    <Icon
+                        raised
+                        reverse
+                        name={props.favorita ? 'heart' : 'heart-o'}
+                        type='font-awesome'
+                        color='#f50'
+                        onPress={() => props.favorita ? console.log('La excursion ya esta en favoritos') : props.onPressFavorito()}
+                    />
+                    <Icon
+                        raised
+                        reverse
+                        name='pencil'
+                        type='font-awesome'
+                        color={colorGaztaroaOscuro}
+                        onPress={() => props.onPressComentar()}
+                    />
+                </View>
             </Card>
             );
         }
@@ -77,24 +87,100 @@ function RenderComentario(props) {
 
 class DetalleExcursion extends Component {
   
+  constructor(props) {
+    super(props);
+    this.state = {
+        rating: 3,
+        autor: '',
+        comentario: '',
+        showModal: false
+    }
+}
 
   marcarFavorito(excursionId) {
     this.props.postFavorito(excursionId);
-    }
+  }
+  resetForm(){
+    this.setState({
+      rating: 3,
+      autor: '',
+      comentario: '',
+      showModal: false
+    });
+  }
 
+  gestionarComentario(excursionId){
+    const fecha = new Date(Date.now()).toISOString()
+    const comentario = {
+      id: null,
+      excursionId: excursionId,
+      valoracion: this.state.rating,
+      autor: this.state.autor,
+      comentario: this.state.comentario,
+      dia: fecha
+    }
+    this.props.postComentario(comentario)
+    this.resetForm()
+  }
+
+
+  toggleModal(){
+    this.setState({
+      showModal: !this.state.showModal
+    })
+}
   render(){
     const {excursionId} = this.props.route.params;
     return(
-        <ScrollView>
-            <RenderExcursion
-                excursion={this.props.excursiones.excursiones[+excursionId]}
-                favorita={this.props.favoritos.some(el => el === excursionId)}
-                onPress={() => this.marcarFavorito(excursionId)}
+      <ScrollView>
+        <RenderExcursion
+          excursion={this.props.excursiones.excursiones[+excursionId]}
+          favorita={this.props.favoritos.some(el => el === excursionId)}
+          onPressFavorito={() => this.marcarFavorito(excursionId)}
+          onPressComentar={() => this.toggleModal()}
+        />
+        <RenderComentario
+            comentarios={this.props.comentarios.comentarios.filter((comentario) => comentario.excursionId === excursionId)}
+        />
+        <Modal
+          animationType = {"slide"}
+          transparent = {false}
+          visible = {this.state.showModal}
+          onDismiss = {() => {this.toggleModal(); this.resetForm();}}
+          onRequestClose = {() => {this.toggleModal(); this.resetForm();}}>
+          <View style = {styles.modal}>
+            <Rating
+              showRating
+              startingValue={3}
+              onFinishRating={rating => this.setState({rating: rating})}
             />
-            <RenderComentario
-                comentarios={this.props.comentarios.comentarios.filter((comentario) => comentario.excursionId === excursionId)}
+            <Input
+              placeholder='Autor'
+              leftIcon={{ type: 'font-awesome', name: 'user' }}
+              leftIconContainerStyle={{marginRight: 15}}
+              onChangeText={value => this.setState({autor: value})}
             />
-        </ScrollView>
+            <Input
+              placeholder='Comentario'
+              leftIcon={{ type: 'font-awesome', name: 'comment' }}
+              leftIconContainerStyle={{marginRight: 15}}
+              onChangeText={value => this.setState({comentario: value})}
+
+            />
+            <Button
+              onPress = {() => this.gestionarComentario(excursionId)}
+              style={{backgroundColor: '#fff', color: colorGaztaroaOscuro}}
+              title="Enviar"
+              type='clear'
+            />
+            <Button
+              onPress = {() =>{this.toggleModal(); this.resetForm();}}
+              title="Cancelar"
+              type='clear'
+            />
+          </View>
+        </Modal>
+      </ScrollView>
     );
   } 
 }
