0631631d2728503aa3d60355fce85884cc32daac
Nerea <nerea.im1896@gmail.com>
2021-06-17 20:35:50 +0200

    Firebase upload: comentarios

--- redux/comentarios.js
+++ redux/comentarios.js
@@ -1,4 +1,6 @@
 import * as ActionTypes from './ActionTypes';
+import { baseUrl } from '../comun/comun';
+
 
 export const comentarios = (state = { errMess: null, comentarios: [] }, action) => {
   switch (action.type) {
@@ -8,11 +10,21 @@ export const comentarios = (state = { errMess: null, comentarios: [] }, action) 
     case ActionTypes.COMENTARIOS_FAILED:
       return { ...state, errMess: action.payload };
 
-      case ActionTypes.ADD_COMENTARIO:
-        action.payload.id = state.comentarios.length
-        state.comentarios.push(action.payload)
-        return { ...state, errMess: null, comentarios: state.comentarios }
-    default:
-      return state;
+    case ActionTypes.ADD_COMENTARIO:
+          action.payload.id = state.comentarios.length
+          state.comentarios.push(action.payload)
+          fetch(baseUrl + 'comentarios/' + action.payload.id + '.json', {
+              method: 'PUT',
+              headers: {
+                  Accept: 'application/json',
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(action.payload)
+          })
+
+          return {...state, errMess: null, comentarios: state.comentarios }
+
+      default:
+          return state;
   }
 };
